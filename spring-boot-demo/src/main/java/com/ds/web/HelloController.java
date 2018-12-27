fdb8f24aab0b4205f8327dfe530145b8cdd3c054
dongsu-iis <dongsu.workout@outlook.jp>
2018-12-28 03:14:09 +0900

    Update HelloController.java

--- spring-boot-demo/src/main/java/com/ds/web/HelloController.java
+++ spring-boot-demo/src/main/java/com/ds/web/HelloController.java
@@ -1,12 +1,16 @@
 package com.ds.web;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
 
-    @RequestMapping("/say")
+    // @RequestMapping(value = "/say",method = RequestMethod.GET)
+    @PostMapping("/say") // 略
     public String hello(){
         return "Hello Spring Boot";
     }
