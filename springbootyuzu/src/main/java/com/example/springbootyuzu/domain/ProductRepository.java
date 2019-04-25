97d8b34ab29c9a8d98b87c12845af404e8b57e06
dongsu-iis <dongsu.workout@outlook.jp>
2019-04-26 00:39:33 +0900

    Hさん用
    検証用に作ってみた

---
+++ springbootyuzu/src/main/java/com/example/springbootyuzu/domain/ProductRepository.java
@@ -0,0 +1,5 @@
+package com.example.springbootyuzu.domain;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer>{
+}
