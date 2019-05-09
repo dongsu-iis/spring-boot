5765a8c7ea8ca288091e04edf8c0606223daf80b
dongsu-iis <dongsu.workout@outlook.jp>
2019-05-09 16:12:38 +0900

    docker対応
    MySQLをXamppからDockerに変更

--- spring-boot-demo/src/main/java/com/ds/domain/Book.java
+++ spring-boot-demo/src/main/java/com/ds/domain/Book.java
@@ -1,6 +1,8 @@
 package com.ds.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter @Setter
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Book {
 
@@ -25,45 +28,5 @@ public class Book {
 
     public Book(){
 
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 }
