011bf53fefb75a67338ed712b9ac825fcc11d22f
dongsu-iis <dongsu.workout@outlook.jp>
2019-05-09 17:43:10 +0900

    docker

--- springbootyuzu/src/main/java/com/example/springbootyuzu/domain/Product.java
+++ springbootyuzu/src/main/java/com/example/springbootyuzu/domain/Product.java
@@ -6,8 +6,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter @Setter
 @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 public class Product {
 
@@ -20,6 +23,7 @@ public class Product {
 
     @Column(nullable = false)
     private Integer supplierId;
+
     private Integer categoryId;
     private String quantityPerUnit;
     private Integer unitPrice;
@@ -28,81 +32,5 @@ public class Product {
     private Integer reorderLevel;
     private Integer discontinued;
 
-    @Column(nullable = false)
-    public void setProductId(Integer productId) {
-        this.productId = productId;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public Integer getSupplierId() {
-        return supplierId;
-    }
-
-    public void setSupplierId(Integer supplierId) {
-        this.supplierId = supplierId;
-    }
-
-    public Integer getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Integer categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getQuantityPerUnit() {
-        return quantityPerUnit;
-    }
-
-    public void setQuantityPerUnit(String quantityPerUnit) {
-        this.quantityPerUnit = quantityPerUnit;
-    }
-
-    public Integer getUnitPrice() {
-        return unitPrice;
-    }
-
-    public void setUnitPrice(Integer unitPrice) {
-        this.unitPrice = unitPrice;
-    }
-
-    public Integer getUnitsInStock() {
-        return unitsInStock;
-    }
-
-    public void setUnitsInStock(Integer unitsInStock) {
-        this.unitsInStock = unitsInStock;
-    }
-
-    public Integer getUnitsOnOrder() {
-        return unitsOnOrder;
-    }
-
-    public void setUnitsOnOrder(Integer unitsOnOrder) {
-        this.unitsOnOrder = unitsOnOrder;
-    }
-
-    public Integer getReorderLevel() {
-        return reorderLevel;
-    }
-
-    public void setReorderLevel(Integer reorderLevel) {
-        this.reorderLevel = reorderLevel;
-    }
-
-    public Integer getDiscontinued() {
-        return discontinued;
-    }
-
-    public void setDiscontinued(Integer discontinued) {
-        this.discontinued = discontinued;
-    }
 
 }
