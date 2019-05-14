46314fed2f3b2f210babfa6c10ef5c241773ab92
dongsu-iis <dongsu.workout@outlook.jp>
2019-05-14 17:39:52 +0900

    認証機能作成

--- springbootyuzu/src/main/java/com/example/springbootyuzu/domain/ProductRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.springbootyuzu.domain;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Integer>{
-}
